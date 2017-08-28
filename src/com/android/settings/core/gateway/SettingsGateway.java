9321d849d7156fa891d85881bf04a486f7827859
maxwen <max.weninger@gmail.com>
2017-08-28 17:15:28 +0200

    Global Actions with Advanced Reboot [3/3]
    Change-Id: I601b4fb4a25e528767bcad7861446c2e8cdd377d
    [jaysonedson@gmail.com: Change setting for lockdown]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -161,6 +161,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.colt.enigma.ColtEnigma;
+import com.colt.enigma.fragments.GlobalActionsSettings;
 
 public class SettingsGateway {
 
@@ -310,7 +311,8 @@ public class SettingsGateway {
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
             MediaControlsSettings.class.getName(),
-            ColtEnigma.class.getName()
+            ColtEnigma.class.getName(),
+            GlobalActionsSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -351,5 +353,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.ColtEnigmaActivity.class.getName(),
+            Settings.GlobalActionsSettingsActivity.class.getName(),
     };
             }

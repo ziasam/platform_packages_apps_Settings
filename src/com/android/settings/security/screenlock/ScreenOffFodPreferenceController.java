Evolution-X-AOSP/packages_apps_Settings

Settings: Add screen off fod [2/2]
Change-Id: I7d443192a1929630d9dbe409f23c9aaff0e4c542

src/com/android/settings/security/screenlock/ScreenOffFodPreferenceController.java
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.screenlock;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+
+import com.android.internal.util.evolution.fod.FodUtils;
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.BasePreferenceController;
+
+public class ScreenOffFodPreferenceController extends BasePreferenceController {
+    private static final String KEY_FOD_GESTURE = "fod_gesture";
+    private Context mContext;
+
+    private FingerprintManager fpm;
+    private PackageManager packageManager;
+
+    public ScreenOffFodPreferenceController(Context context) {
+        super(context, KEY_FOD_GESTURE);
+        mContext = context;
+
+        fpm = Utils.getFingerprintManagerOrNull(context);
+        packageManager = context.getPackageManager();
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        if (fpm != null && fpm.isHardwareDetected() && fpm.hasEnrolledFingerprints()
+                && FodUtils.hasFodSupport(mContext)
+                && mContext.getResources().getBoolean(R.bool.config_supportScreenOffFod)) {
+            return AVAILABLE;
+        }
+        return UNSUPPORTED_ON_DEVICE;
+    }
+}

ziasam/platform_packages_apps_Settings

Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

src/com/android/settings/security/screenlock/ScreenOffFodPreferenceController.java
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.security.screenlock;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+
+import com.android.internal.util.colt.fod.FodUtils;
+import com.android.settings.R;
+import com.android.settings.Utils;
+import com.android.settings.core.BasePreferenceController;
+
+public class ScreenOffFodPreferenceController extends BasePreferenceController {
+    private static final String KEY_FOD_GESTURE = "fod_gesture";
+    private Context mContext;
+
+    private FingerprintManager fpm;
+    private PackageManager packageManager;
+
+    public ScreenOffFodPreferenceController(Context context) {
+        super(context, KEY_FOD_GESTURE);
+        mContext = context;
+
+        fpm = Utils.getFingerprintManagerOrNull(context);
+        packageManager = context.getPackageManager();
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        if (fpm != null && fpm.isHardwareDetected() && fpm.hasEnrolledFingerprints()
+                && FodUtils.hasFodSupport(mContext)
+                && mContext.getResources().getBoolean(R.bool.config_supportScreenOffFod)) {
+            return AVAILABLE;
+        }
+        return UNSUPPORTED_ON_DEVICE;
+    }
+}
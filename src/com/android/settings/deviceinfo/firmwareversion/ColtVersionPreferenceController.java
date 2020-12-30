754036dee77ffca1cbbed7dacbe75f5b9a586706
elluzion <dyrex2004@gmail.com>
2020-12-31 00:10:03 +0100

    AboutInfo:Revamp ColtEnigma Version
    Required Flags in DT:
    PRODUCT_PRODUCT_PROPERTIES += \
      ro.colt.maintainer=maintainername \
      ro.colt.cpu=for example SDM855
    
    Co-authored-by: Androbuddy <anirudhchimpidi8@gmail.com>
    Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/ColtVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/ColtVersionPreferenceController.java
@@ -16,33 +16,121 @@
 
 package com.android.settings.deviceinfo.firmwareversion;
 
+import java.io.IOException;
 import android.content.Context;
+import androidx.annotation.VisibleForTesting;
 import android.os.SystemProperties;
 import android.text.TextUtils;
 
 import androidx.annotation.VisibleForTesting;
 
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+import com.android.settings.utils.ColtSpecUtils;
+
 import com.android.settings.R;
-import com.android.settings.Utils;
+import com.android.settingslib.widget.LayoutPreference;
+import com.android.settingslib.Utils;
+import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settings.core.BasePreferenceController;
 
+import android.widget.TextView;
 public class ColtVersionPreferenceController extends BasePreferenceController {
 
     @VisibleForTesting
-    static final String COLT_VERSION_PROPERTY = "ro.colt.release.version";
+
+    private static final String KEY_COLT_VERSION_PROP = "ro.colt.release.version";
+
+    @VisibleForTesting
+    TextView mColtVersionText;
+    @VisibleForTesting
+    TextView mColtVersionFlavourText;
+    @VisibleForTesting
+    TextView mDeviceNameText;
+    @VisibleForTesting
+    TextView mCpuText;
+    @VisibleForTesting
+    TextView mScreenResText;
+    @VisibleForTesting
+    TextView mBatteryText;
+    @VisibleForTesting
+    TextView mRamText;
+    @VisibleForTesting
+    TextView mMaintainerText;
+
+    private PreferenceFragmentCompat mHost;
+    private LayoutPreference mColtVersionLayoutPref;
+    private Context mContext;
 
     public ColtVersionPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
+        mContext = context;
     }
 
     @Override
     public int getAvailabilityStatus() {
-        return !TextUtils.isEmpty(SystemProperties.get(COLT_VERSION_PROPERTY)) ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return AVAILABLE;
+    }
+
+    public void setFragment(PreferenceFragmentCompat fragment) {
+        mHost = fragment;
     }
 
     @Override
-    public CharSequence getSummary() {
-        return SystemProperties.get(COLT_VERSION_PROPERTY,
-                mContext.getString(R.string.device_info_default));
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mColtVersionLayoutPref = screen.findPreference(getPreferenceKey());
+        mColtVersionText = mColtVersionLayoutPref.findViewById(R.id.colt_version);
+        mColtVersionFlavourText = mColtVersionLayoutPref.findViewById(R.id.colt_version_flavour);
+        mDeviceNameText = mColtVersionLayoutPref.findViewById(R.id.device_name_text);
+        mCpuText = mColtVersionLayoutPref.findViewById(R.id.device_cpu_text);
+        mScreenResText = mColtVersionLayoutPref.findViewById(R.id.device_screen_res_text);
+        mBatteryText = mColtVersionLayoutPref.findViewById(R.id.device_battery_text);
+        mRamText = mColtVersionLayoutPref.findViewById(R.id.device_ram_text);
+        mMaintainerText = mColtVersionLayoutPref.findViewById(R.id.device_maintainer_text);
+
+        UpdateColtVersionPreference();
     }
+
+    private void UpdateColtVersionPreference() {
+        // We split the different specs into different voids to make the code more organized.
+        updateColtVersionText();
+        updateDeviceNameText();
+        updateCpuText();
+        updateScreenResText();
+        updateBatteryText();
+        updateRamText();
+        updateMaintainerText();
+
+    }
+
+    public CharSequence updateColtVersionText() {
+      return SystemProperties.get(KEY_COLT_VERSION_PROP,
+                mContext.getString(R.string.unknown));
+
+    }
+
+    private void updateDeviceNameText() {
+        mDeviceNameText.setText(ColtSpecUtils.getDeviceName());
+    }
+
+    private void updateBatteryText() {
+        mBatteryText.setText(ColtSpecUtils.getBatteryCapacity(mContext) + " mAh");
+    }
+
+    private void updateCpuText() {
+        mCpuText.setText(ColtSpecUtils.getProcessorModel());
+    }
+
+    private void updateScreenResText() {
+        mScreenResText.setText(ColtSpecUtils.getScreenRes(mContext));
+    }
+
+    private void updateRamText() {
+        mRamText.setText(String.valueOf(ColtSpecUtils.getTotalRAM())+ " GB");
+    }
+    private void updateMaintainerText() {
+        mMaintainerText.setText(String.valueOf(ColtSpecUtils.getMaintainerName()));
+    }
+
 }
